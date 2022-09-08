f237740975efa840966f79c766185a6b44856739
faithkangui <faithkangui@gmail.com>
2022-09-08 12:58:24 -0700

    size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	printf("Size of a char:  %zu byte(s)\n", sizeof(char Type));
+	printf("Size of a char:  %d byte(s)\n", sizeof(char));
 	printf("Size of an int:  %zu byte(s)\n", sizeof(int Type));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int Type));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long Type));
