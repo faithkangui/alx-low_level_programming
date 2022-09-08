f800801c16234070514241a5dc6c1f8fa1d19c06
faithkangui <faithkangui@gmail.com>
2022-09-08 11:06:33 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * print a text
+ * main - Entry point
  *
- * Return 0
+ * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
-}	
+	return (0);
+}
