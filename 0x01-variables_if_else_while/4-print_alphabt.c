fa81de82d843be3420c24f57d5cfe6968f384205
faithkangui <faithkangui@gmail.com>
2022-09-09 08:37:19 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,8 @@ int main(void)
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		if (ch != 'e' && ch != 'q')
-		       putchar(ch);
-	}
+	{if (ch != 'e' && ch != 'q')}
+	putchar(ch);
 	putchar('\n');
 	return (0);
 }
