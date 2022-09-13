e088d39bd096dfca3caa7bda86c24aa61ccbdd3f
faithkangui <faithkangui@gmail.com>
2022-09-13 06:47:57 -0700

    lowercase alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,9 +7,16 @@
  *Return: Always 0.
  */
 int main(void)
+void ch(void);
 {
 	int ch;
 	char str[] = "_putchar";
 	putchar(str[ch]);
 	return (0);
+}
+void print_alphabet(void);
+{
+	int print_alphabet = ch;
+	print _alphabet();
+	return (0)i;
 }
