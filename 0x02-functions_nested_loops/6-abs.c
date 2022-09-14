44aa298918351b6fa60400880a819f562b6be745
faithkangui <faithkangui@gmail.com>
2022-09-14 11:59:31 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * int _abs - computes the absolute valueee of an integer
+ *
+ *@a: int
+ *
+ * Return: int
+ */
+int _abs(int a)
+{
+	int n;
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
