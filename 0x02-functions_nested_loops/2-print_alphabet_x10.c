2c251095b8eb7a3aad5b304a405f0facfc46f04b
faithkangui <faithkangui@gmail.com>
2022-09-14 08:44:21 -0700

    10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet ten times
+ *
+ * Return: void
+ */
+int print_alphabet_x10(void)
+{
+	int ch;
+	int n;
+	
+	for (n =  0; n < 10; n++)
+	{
+		for (ch = 'a'; ch < 'z'; ch++)
+		{
+		_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
