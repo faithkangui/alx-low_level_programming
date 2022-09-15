67ff64e01889ff8dfcdb67cfed1c9dfe35497bb7
faithkangui <faithkangui@gmail.com>
2022-09-15 03:04:31 -0700

    checks for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ *
+ *@c: integer to print
+ *
+ * Return: int
+ */
+int _isdigit(int c)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == c)
+		return (1);
+	}
+	return (0);
+}
