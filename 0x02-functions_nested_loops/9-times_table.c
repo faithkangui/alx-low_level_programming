ab8a141e29cc90ceeee3cf6d7d93a32bfb71a734
faithkangui <faithkangui@gmail.com>
2022-09-15 06:44:23 -0700

    9 timetable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 time table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			_putchar(a * b);
+			_putchar(,);
+		}
+		_putchar('\n');
+	}
+}
