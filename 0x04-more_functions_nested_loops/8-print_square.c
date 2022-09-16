dd4f3fef27ddea5908e6045dda3d3ad31e0dc89c
faithkangui <faithkangui@gmail.com>
2022-09-16 07:32:45 -0700

    print a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,12 +11,12 @@ void print_square(int size)
 	int i;
 	int k;
 
-	for (i = 0: i <= size; i++)
+	for (i = 0; i <= size; i++)
 	{
 		for (j = 0; j <= size; j++)
 		{
 			if (size > 0)
-			{ 
+			{
 				_putchar('\n');
 			}
 			else
