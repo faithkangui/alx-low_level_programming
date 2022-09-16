b697e248bdc2a9d081d9cb576aaf776e0e7c1bb3
faithkangui <faithkangui@gmail.com>
2022-09-15 23:49:00 -0700

    9 time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,26 @@ void times_table(void)
 {
 	int a;
 	int b;
+	int n;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			_putchar(a * b);
+			n = a * b;
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			else
+				_putchar(n);
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		_putchar(',');
-		_putchar(' ');
+	_putchar('\n');
 	}
 }
