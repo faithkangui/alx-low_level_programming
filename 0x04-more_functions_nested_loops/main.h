f5c233a478bb63ef5aec528cfb17a0d6ab4246d5
faithkangui <faithkangui@gmail.com>
2022-09-15 22:20:01 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,3 +15,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void fizz_buzz(void);
 void print_triangle(int size);
+
+#endif /* MAIN_H */
