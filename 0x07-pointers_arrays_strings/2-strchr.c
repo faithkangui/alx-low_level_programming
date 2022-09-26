6b5a63a89ac015f2ac37ea88cc78dfdbe9f91e77
faithkangui <faithkangui@gmail.com>
2022-09-27 02:36:33 +0300

    locates a string in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates a string of character.
+ * @s: pointer to string
+ * @c: char to check
+ *
+ * Return: pointer to char c or NULL char not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+		s++;
+	return (s);
+	while (c == '\0')
+	{
+	return (s);
+	}
+	return (NULL);
+}
