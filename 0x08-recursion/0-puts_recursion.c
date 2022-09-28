14c702b75b61db6c7497f4028ca00949fac5b5b7
faithkangui <faithkangui@gmail.com>
2022-09-29 00:02:08 +0300

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	s++;
+	{
+		return;
+	}
+}
