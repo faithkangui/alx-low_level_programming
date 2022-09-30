c1a5cd2cd56a76a8b9f661395e243f667bddd0ba
faithkangui <faithkangui@gmail.com>
2022-10-01 00:37:00 +0300

    prints its name

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
+int main(__attribute__((unused))int argc, char *argv[]);
+int main(int argc, __attribute__((unused))char *argv[]);
 #include <stdlib.h>
 #include <stdio.h>
 #endif/* MAIN_H */
