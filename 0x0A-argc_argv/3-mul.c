f640587571b15ba76b036f2d75d950532f2b8c35
faithkangui <faithkangui@gmail.com>
2022-10-01 01:51:35 +0300

    multiplies two number

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - multiplies two numbers
+ * @argc: int
+ * @argv: array
+ *
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int i, mul = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argc < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			mul = i * mul;
+			printf("%d", mul); 
+		}
+	}
+	return (0);
+}
