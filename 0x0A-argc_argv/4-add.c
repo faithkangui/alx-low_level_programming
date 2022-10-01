6ea98b49c6132178db2feb4afd794aef34ccab3d
faithkangui <faithkangui@gmail.com>
2022-10-01 10:51:36 +0300

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * main - adds positive numbers
+ * @argc:int
+ * @argv: an array
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (isalpha(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
