36fa8f4bcda81a7634d3f6d12e8976f05ee421f6
faithkangui <faithkangui@gmail.com>
2022-10-12 19:56:45 +0300

    function-like macro that computes the absolute value

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,9 @@
 #ifndef ABS
-#define ABS(x) ((x<0)?(-x):(x))
-#endif /* ABS */
+#define ABS(x) ((x) < (0) ? (x * -1) : (x))
+/**
+ * ABS - takes integer and returns its absolute value
+ * @x: int
+ *
+ * Return: int
+*/
+#endif/* ABS */
