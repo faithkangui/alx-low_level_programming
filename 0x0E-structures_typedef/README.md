a5680ec220cb6a13fe5bd497bb142fe1a73d0fbe
faithkangui <faithkangui@gmail.com>
2022-10-13 00:01:08 +0300

    define a new type 'struct dog'

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+Structure and Typedef
+- structures are like user defined data types
+- structure are used when you want to have an array of different data types
+- typedef this a keyword in C used to give a type a new name.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+#endif/* DOG_H */
