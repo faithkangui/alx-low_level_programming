88402557dc2da0108a0b155bd3c7c7e9011bc5c8
faithkangui <faithkangui@gmail.com>
2022-11-23 05:53:07 +0300

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,10 +13,15 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int j;
 	va_list valist;
 
+	if (n == 0)
+		return (0);
+
 	va_start(valist, n);
 
 	for (j = 0; j < n; j++)
 		sum += va_arg(valist, int);
+
 	va_end(valist);
+
 	return (sum);
 }
